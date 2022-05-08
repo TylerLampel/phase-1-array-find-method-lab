0080db3a436cd39729b9bdfdaa8c5b527792cb99
Tyler Lampel <lampel13@gmail.com>
2022-05-08 19:14:11 -0400

    commit tree 0db8a33396ee95fbc534ac770d0f4a89880580cc

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(record) {
+    const resultFoo = function(record){
+        if (record.result === "W"){
+            return record.year
+        }
+    }
+    if (record.find(resultFoo) === undefined) {
+    return undefined
+    } else {
+        return record.find(resultFoo).year
+    }
+}
